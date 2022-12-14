5436857af855af0c7450a8da0843d675e90aac31
Dinakara Sai Santosh Burugupalli <53957775+dburugupalli@users.noreply.github.com>
2022-12-14 05:33:53 -0800

    Update routes.go

--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -8,10 +8,10 @@ func (a *App) initializeRoutes() {
 	var apiV1 = "/api/v1"
 
 	// specification routes
-	a.Router.HandleFunc(apiV2+"/specification/list", a.listConnectorSpecification).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc(apiV1+"/specification/list", a.listConnectorSpecification).Methods("GET", "OPTIONS")
 
 	// user routes
-	a.Router.HandleFunc(apiV2+"/admin/v1/users", a.getUsers).Methods("GET", "OPTIONS")
+	a.Router.HandleFunc(apiV1+"/admin/v1/users", a.getUsers).Methods("GET", "OPTIONS")
 
 	// Swagger
 	a.Router.PathPrefix("/docs").Handler(httpSwagger.WrapHandler)
